cf8e0ff492e980c9d7690001c6d0b0097a2d7f1e
Feras <ferasit87@gmail.com>
2017-12-29 15:37:22 +0300

    Change in Exaples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,8 @@ const React = require('react');
 
 var Examples = (props) => {
   return (
-      <h3> Examples component </h3>
+      <h3>Examples</h3>
+      <p>Welcome to examples page!</p>
     );
 }
 module.exports = Examples;
